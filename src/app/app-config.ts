9ade42beccd556eb1ee4bd67ae5c6ce8782bdcd2
Diego Noveron <diego12345781@hotmail.com>
2022-04-25 04:38:21 -0500

    Cambiar logos

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -16,9 +16,9 @@ import { CoreConfig } from '@core/types';
 // prettier-ignore
 export const coreConfig: CoreConfig = {
   app: {
-    appName     : 'Vuexy',                                        // App Name
-    appTitle    : 'Vuexy - Angular 12+ Bootstrap Admin Template', // App Title
-    appLogoImage: 'assets/images/logo/logo.svg',                  // App Logo
+    appName     : 'Parking',                                        // App Name
+    appTitle    : '', // App Title
+    appLogoImage: '',                  // App Logo
     appLanguage : 'en',                                           // App Default Language (en, fr, de, pt etc..)
   },
   layout: {
